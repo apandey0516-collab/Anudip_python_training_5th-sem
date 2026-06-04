df3604ddb6c79b53735940f1b4ce8a9bcbeefd30
apandey0516-collab <apandey0516@gmail.com>
2026-06-04 14:21:52 +0530

    armstrong

---
+++ lab_work/iteration 4thjune/Armstrong Number checker.py
@@ -0,0 +1,12 @@
+#program to display armstrong number checker
+num = int(input("enter a number:")) 
+sum =0
+temp = num
+while temp > 0: 
+    digit = temp % 10
+    sum += digit ** 3
+    temp //= 10
+if sum == num:
+    print(num, "is an Armstrong number")
+else:
+    print(num, "is not an Armstrong number")
