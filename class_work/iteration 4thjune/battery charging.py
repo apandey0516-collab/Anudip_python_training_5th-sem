a99a520f9b9567ed568440c52998ac086c68e39e
apandey0516-collab <apandey0516@gmail.com>
2026-06-04 23:34:15 +0530

    charging level

---
+++ class_work/iteration 4thjune/battery charging.py
@@ -0,0 +1,9 @@
+#program for displaying battery charging level
+battery_level = 20
+electricity_status = True
+while(battery_level <=100):
+    print("battery level is: ", battery_level, "%")
+    battery_level = battery_level + 10
+#-----------------------------------------------------------------------
+print("battery is fully charged")
+
