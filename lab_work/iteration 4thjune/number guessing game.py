24f41c268b78e02584a37fd17f70721493bab76a
apandey0516-collab <apandey0516@gmail.com>
2026-06-05 09:57:30 +0530

    pattern

--- lab_work/iteration 4thjune/number guessing game.py
+++ lab_work/iteration 4thjune/number guessing game.py
@@ -3,7 +3,7 @@ import random
 number_to_guess = random.randint(1, 100) #to generate a random number between 1 and 100 for the user to guess   
 attempts = 0#to initialize the attempts variable to keep track of the number of guesses the user has made
 max_attempts = 10 #to set a maximum number of attempts to prevent infinite guessing and to add a challenge to the game9
-print("Welcome to the Number Guessing Game!")
+print("Welcome to the Number Guessing Game!") 
 print("I have selected a number between 1 and 100. Can you guess it?")
 while attempts < max_attempts:
     try:
