0e177f78d3be3385707c865efd7c12011877cba5
apandey0516-collab <apandey0516@gmail.com>
2026-06-12 11:50:42 +0530

    .

---
+++ class_work/iteration .py
@@ -0,0 +1,6 @@
+## Create a function that accepts a string and returns the count of vowels and consonants using iteration statements.
+voewels = "a,,e ,i,o,u"
+consonats =[]
+count = 0
+for i in range 
+print(" enter the vowels and consonats")
